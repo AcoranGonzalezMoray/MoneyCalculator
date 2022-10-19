cb5bc2a2a14b9d01bbdba77007e7814698526538
AcoranGonzalezMoray <acoranlol14@gmail.com>
2022-10-19 15:26:40 +0100

    versión 2

--- src/Control/MoneyCalculator.java
+++ src/Control/MoneyCalculator.java
@@ -9,23 +9,22 @@ import java.util.List;
 public class MoneyCalculator {
 
     public static void main(String[] args){
-    
+        //MODEL
         CurrencyLoaderFromFile currencyLoaderFromFile = new CurrencyLoaderFromFile ("Curren.txt");
         List<Currency> list =currencyLoaderFromFile.loadCurrencies();
-        for(Currency currency : list ){
-            System.out.println(currency.getCode());
-        }
         ExchangeRateLoaderFromWebService exchangeRateLoaderFromWebService = new ExchangeRateLoaderFromWebService() ;
         for(Currency currencyFrom : list){
-            
             for(Currency currencyTo :list){
-                if (currencyFrom.getCode().equals(currencyTo.getCode())){
+                if (currencyFrom.getCode().compareTo(currencyTo.getCode())!=0){
                     ExchangeRate exchangeRate =  exchangeRateLoaderFromWebService.exh(currencyFrom, currencyTo);
-                    //System.out.println(exchangeRateLoaderFromWebService.read(currencyFrom.getCode(), currencyTo.getCode()));
+                    System.out.println("Rate from " + exchangeRate.getCurrency_front().getCode() + " to "+ exchangeRate.getCurrency_to().getCode() + " RATE: "+exchangeRate.getRate());
                 }
             }
         
         }
+        
+        //VIEW
+        
     }
     
 }
--- src/Persistence/rest/ExchangeRateLoaderFromWebService.java
+++ src/Persistence/rest/ExchangeRateLoaderFromWebService.java
@@ -2,8 +2,10 @@ package Persistence.rest;
 
 import Model.Currency;
 import Model.ExchangeRate;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -12,15 +14,17 @@ public class ExchangeRateLoaderFromWebService implements ExchangeRateLoader{
 
     @Override
     public ExchangeRate exh(Currency from, Currency to) {
-        return new ExchangeRate(from, to, read(from.getCode(), to.getCode()));
+        return new ExchangeRate(from, to, read(from.getName(), to.getName()));
     }
 
     private double read(String codeFrom, String codeTo) {
         try{
-            String line = read(new URL("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"+codeFrom+"/"+codeTo+".json"));
+            
+            String line = read2(new URL("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"+codeFrom+"/"+codeTo+".json"));
             return Double.parseDouble(getStringRateFromJSONLine(line));
             //more code goes here
         }catch(MalformedURLException ex){
+            System.out.println(ex);
         }catch( IOException ex){}
         
         
@@ -29,7 +33,7 @@ public class ExchangeRateLoaderFromWebService implements ExchangeRateLoader{
 
     private String getStringRateFromJSONLine(String line) {
         String[] split = line.split(",");
-        return split[1].substring(split[1].indexOf(":")+1,split[1].indexOf(")")-1 );
+        return split[1].split(":")[1].split("\n")[0];
      }
     
     private String read(URL url) throws IOException{
@@ -38,5 +42,20 @@ public class ExchangeRateLoaderFromWebService implements ExchangeRateLoader{
         int lenght = inputStream.read(buffer);
         return new String(buffer,1,lenght);
     }
-    
+    private String read2(URL url){
+        String line="";
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuilder sb = new StringBuilder();
+            int cp;
+            while ((cp = in.read()) != -1)sb.append((char) cp);
+            return sb.toString();
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return "";
+    }
 }
